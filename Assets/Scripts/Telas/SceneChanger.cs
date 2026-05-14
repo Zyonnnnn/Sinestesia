5ca0e172b9bf2cdd9d3d272b05f88b7a3f578590
Juliana Leão <julianaleao.tecnico@gmail.com>
2026-05-14 11:40:10 -0300

    Pause Canva
    If you click esc you pause the entire game

--- Assets/Scripts/Telas/SceneChanger.cs
+++ Assets/Scripts/Telas/SceneChanger.cs
@@ -1,10 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public GameObject uiImage;
     public void SceneChange(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void Awake()
+    {
+        uiImage.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            uiImage.SetActive(!uiImage.activeSelf);
+
+            Time.timeScale = uiImage.activeSelf ? 0f : 1f;
+        }
     }
 }
