d4b8b47d1a7840e44033690599d8693eb5049a15
Juliana Leão <julianaleao.tecnico@gmail.com>
2026-07-14 14:23:01 -0300

    Menu
    omg im still here

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -8,24 +8,30 @@ public class SceneChanger : MonoBehaviour
 {
     public GameObject uiImage;
     public GameObject pauseMenu;
+    public GameObject mMenu;
+    public GameObject cMenu;
 
     private void Awake()
     {
         Time.timeScale = 1.0f;
         uiImage = GameObject.FindGameObjectWithTag("PauseImg");
-        pauseMenu = GameObject.FindGameObjectWithTag("Buttons");
+        mMenu = GameObject.FindGameObjectWithTag("MainMenu");
+        cMenu = GameObject.FindGameObjectWithTag("ConfigMenu");
     }
 
     private void Start()
     {
+        if (mMenu != null && cMenu != null)
+        {
+            mMenu.SetActive(true);
+            cMenu.SetActive(false);
+        }
+        
         if (uiImage != null)
         {
             uiImage.SetActive(false);
         }
-        if (pauseMenu != null)
-        {
-            pauseMenu.SetActive(false);
-        }
+        
     }
     private void Update()
     {
@@ -44,8 +50,19 @@ public class SceneChanger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape) && uiImage != null)
         {
             uiImage.SetActive(!uiImage.activeSelf);
+            mMenu.SetActive(true);
+            cMenu.SetActive(false);
 
             Time.timeScale = uiImage.activeSelf ? 0f : 1f;
+        }
+    }
+    
+    public void ConfigMenu()
+    {
+        if (mMenu != null && cMenu != null)
+        {
+            mMenu.SetActive(!mMenu.activeSelf);
+            cMenu.SetActive(!cMenu.activeSelf);
         }
     }
 
