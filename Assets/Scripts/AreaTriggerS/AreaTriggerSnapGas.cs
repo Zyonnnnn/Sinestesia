e397c775524ef5a4db274fa57771bbe31fa9d0c8
zyonn <uitryesra@gmail.com>
2026-09-24 10:39:36 -0300

    start sinestesy place gas

---
+++ Assets/Scripts/AreaTriggerS/AreaTriggerSnapGas.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class AreaTriggerSnapGas : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    private void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        ps.Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GasCollision"))
+        {
+            other.transform.position = new Vector3(37.25f, 1.5f, 20f);
+        }
+    }
+}
